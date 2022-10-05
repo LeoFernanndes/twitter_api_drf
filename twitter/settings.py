216926c1e869b4fd0bba927a7575d5ba32f27485
LeoFernanndes <balbino.leonel@gmail.com>
2022-10-05 01:03:51 -0300

    drf database setup

--- twitter/settings.py
+++ twitter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,13 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config("DB_DEV_NAME"),
+        "USER": config("DB_DEV_USER"),
+        "PASSWORD": config("DB_DEV_PASSWORD"),
+        "HOST": config("DB_DEV_HOST"),
+        "PORT": config("DB_DEV_PORT"),
     }
 }
 
