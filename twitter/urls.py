7bfaebaf4497f94e0981fcb412e671631a99149a
LeoFernanndes <balbino.leonel@gmail.com>
2022-10-05 02:35:29 -0300

    adds swagger drf-yasg as api documentation

--- twitter/urls.py
+++ twitter/urls.py
@@ -13,10 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mais Todos API",
+        default_version='v1',
+        description="Api  for tweets backup management.",
+        contact=openapi.Contact(email="contato@leobalbino.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include("users.routes"), name="users"),
+    url('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
